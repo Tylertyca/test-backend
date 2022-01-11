88f95ed3edf32e8b75324cfa609abfea8ade1c5f
Tyler <tylertycahart@gmail.com>
2022-01-11 11:22:11 -0800

    added delete function

--- backend.js
+++ backend.js
@@ -68,7 +68,30 @@ function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
 }
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
 
+function addUser(user){
+    users['users_list'].push(user);
+}
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        //result = {users_list: result};
+        users['users_list'] = deleteUser(id);
+        res.status(200).end();
+    }
+});
+
+const deleteUser = (id) =>{
+    return users['users_list'].filter( (user) => user['id'] != id);
+}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
